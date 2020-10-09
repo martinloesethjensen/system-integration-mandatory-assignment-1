a639fe4e39687d4225a48b38f84350b07a5c5712
AndoPatrik <andopatrik0@gmail.com>
2020-10-09 14:48:03 +0200

    Added alternative GO implementation for UserGenerator

---
+++ NemID_UserGenerator/User Generator GO/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"math/rand"
+	"strings"
+	"strconv"
+)
+
+
+type User struct {
+	CPR    string  `json:"cpr"`
+	Email   string  `json:"email"`
+}
+
+type NemID struct {
+	NemID	string	`json:nemId`
+}
+
+func main(){
+	fmt.Println("NemID User Generator running on port 8088...")
+	r:= mux.NewRouter()
+	r.HandleFunc("/generate-nemID", GenerateNemID).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8088", r))
+}
+
+func GenerateNemID(w http.ResponseWriter, r *http.Request) {
+	var user User
+	var response NemID
+	var str strings.Builder
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewDecoder(r.Body).Decode(&user)
+
+    for i := 0; i < 5; i++ {
+        str.WriteString(strconv.Itoa(rand.Intn(9-0 + 1) + 0))
+    }
+
+	response.NemID = str.String() + "-" + string(user.CPR[len(user.CPR)-4:])
+	json.NewEncoder(w).Encode(response)
+}
